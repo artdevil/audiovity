15e4b3c730e7c6962563f07f1cc81b45735853c9
art devil <artdvl91@gmail.com>
2012-03-31 03:12:04 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Audiolist::Application.config.secret_token = 'a981b05b539a66319d2371dd7855b2263b5a48e09912ea91ae2fa1f4cb7ad3c63c1bcc79cab618ef5ea5e051b1857ef809233a2a211264e8eba5827615494cb0'
